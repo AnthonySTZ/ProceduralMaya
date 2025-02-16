e44aeff2471b3e36611afb39f5db212b3823e652
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-16 13:55:57 +0100

    reloadNodes and getNodes in NodesInfo

--- src/Core/Nodes/NodesInfo.py
+++ src/Core/Nodes/NodesInfo.py
@@ -1,2 +1,17 @@
 class NodesInfo:
-    pass
+
+    @staticmethod
+    def getNodes():
+        from Core.Nodes.Cube import Cube
+
+        nodes = [
+            Cube,
+        ]
+        return nodes
+
+    @staticmethod
+    def reloadNodes():
+        from importlib import reload
+        import Core.Nodes.Cube as Cube
+
+        reload(Cube)
