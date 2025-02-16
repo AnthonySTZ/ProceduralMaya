46300eff17eca286dc4aa1eabf63b6948689ce3e
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-16 20:12:26 +0100

    rename _settings to parameter for the cube Node

--- src/Core/Nodes/Cube.py
+++ src/Core/Nodes/Cube.py
@@ -6,9 +6,9 @@ class Cube(BaseNode):
         super().__init__()
         self._num_inputs = 0
         self._num_outputs = 1
-        self._settings = {"width": 1.0, "height": 1.0, "depth": 1.0}
+        self._parameters = {"width": 1.0, "height": 1.0, "depth": 1.0}
 
     def getOutput(self, output_index):
         if not (0 <= output_index < self._num_outputs):
             raise NotImplementedError("Invalid output index " + str(output_index))
-        return "Create Cube command with : " + str(self._settings)
+        return "Create Cube command with : " + str(self._parameters)
