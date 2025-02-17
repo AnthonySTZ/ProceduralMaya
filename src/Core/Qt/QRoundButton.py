30f576e9bc73bb921693e9638bc4aab9f6fabaff
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-17 13:53:19 +0100

    create a QRoundButton

---
+++ src/Core/Qt/QRoundButton.py
@@ -0,0 +1,12 @@
+from PluginLib.CompactQt.Qt import QPushButton
+
+
+class QRoundButton(QPushButton):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def setButtonSize(self, size):
+        self.setFixedSize(size, size)
+        self.setStyleSheet(
+            "QPushButton{border-radius:" + str(size / 2) + ";border: 1px solid black;}"
+        )
