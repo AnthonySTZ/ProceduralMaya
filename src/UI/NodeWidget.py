0b2200407dd7bf5dcb3713377b72f032a7cb9bb8
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-17 13:53:30 +0100

    display inputs and outputs buttons

--- src/UI/NodeWidget.py
+++ src/UI/NodeWidget.py
@@ -1,4 +1,13 @@
-from PluginLib.CompactQt.Qt import QWidget, QVBoxLayout, QLabel, QHBoxLayout, Qt
+from PluginLib.CompactQt.Qt import (
+    QWidget,
+    QVBoxLayout,
+    QLabel,
+    QHBoxLayout,
+    Qt,
+    QPushButton,
+)
+
+from Core.Qt.QRoundButton import QRoundButton
 
 
 class NodeWidget(QWidget):
@@ -10,15 +19,34 @@ class NodeWidget(QWidget):
         main_vbox = QVBoxLayout()
         self.setLayout(main_vbox)
 
+        self.inputs_hbox = QHBoxLayout()
+        inputs_widget = QWidget()
+        inputs_widget.setLayout(self.inputs_hbox)
+        main_vbox.addWidget(inputs_widget)
+
         main_rect = QWidget()
         rect_hbox = QHBoxLayout()
         main_rect.setLayout(rect_hbox)
+        main_vbox.addWidget(main_rect)
 
         self.nodeName = QLabel("Node Name")
         self.nodeName.setAlignment(Qt.AlignmentFlag.AlignCenter)
         rect_hbox.addWidget(self.nodeName)
 
-        main_vbox.addWidget(main_rect)
+        self.outputs_hbox = QHBoxLayout()
+        outputs_widget = QWidget()
+        outputs_widget.setLayout(self.outputs_hbox)
+        main_vbox.addWidget(outputs_widget)
 
     def setNode(self, node):
         self.nodeName.setText(node.getName())
+
+        for _ in range(node.getNumberOfInputs()):
+            input_btn = QRoundButton()
+            input_btn.setButtonSize(10)
+            self.inputs_hbox.addWidget(input_btn)
+
+        for _ in range(node.getNumberOfOutputs()):
+            output_btn = QRoundButton()
+            output_btn.setButtonSize(10)
+            self.outputs_hbox.addWidget(output_btn)
