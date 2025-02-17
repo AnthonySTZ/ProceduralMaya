e5fa7f977845920d5489ca1f3a073db01aab22cd
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-17 14:11:43 +0100

    Node UI

--- src/UI/NodeWidget.py
+++ src/UI/NodeWidget.py
@@ -4,7 +4,7 @@ from PluginLib.CompactQt.Qt import (
     QLabel,
     QHBoxLayout,
     Qt,
-    QPushButton,
+    QFrame,
 )
 
 from Core.Qt.QRoundButton import QRoundButton
@@ -17,36 +17,61 @@ class NodeWidget(QWidget):
 
     def buildUI(self):
         main_vbox = QVBoxLayout()
+        main_vbox.setContentsMargins(0, 0, 0, 0)
         self.setLayout(main_vbox)
 
         self.inputs_hbox = QHBoxLayout()
-        inputs_widget = QWidget()
-        inputs_widget.setLayout(self.inputs_hbox)
-        main_vbox.addWidget(inputs_widget)
+        self.inputs_hbox.setContentsMargins(0, 0, 0, 0)
+        self.inputs_widget = QWidget()
+
+        self.inputs_widget.setLayout(self.inputs_hbox)
+        main_vbox.addWidget(self.inputs_widget)
 
         main_rect = QWidget()
         rect_hbox = QHBoxLayout()
+        rect_hbox.setContentsMargins(0, 0, 0, 0)
         main_rect.setLayout(rect_hbox)
+        main_rect.setFixedHeight(30)
         main_vbox.addWidget(main_rect)
+        main_rect.setStyleSheet(
+            """
+                QWidget {
+                    background-color: #dbdbdb;
+                    color: black;
+                    border-radius: 8px;
+                }
+            """
+        )
 
         self.nodeName = QLabel("Node Name")
         self.nodeName.setAlignment(Qt.AlignmentFlag.AlignCenter)
         rect_hbox.addWidget(self.nodeName)
 
         self.outputs_hbox = QHBoxLayout()
-        outputs_widget = QWidget()
-        outputs_widget.setLayout(self.outputs_hbox)
-        main_vbox.addWidget(outputs_widget)
+        self.outputs_hbox.setContentsMargins(0, 0, 0, 0)
+        self.outputs_widget = QWidget()
+        self.outputs_widget.setLayout(self.outputs_hbox)
+        main_vbox.addWidget(self.outputs_widget)
 
     def setNode(self, node):
+        self.inputs_widget.setHidden(True)
+        self.outputs_widget.setHidden(True)
+
         self.nodeName.setText(node.getName())
 
-        for _ in range(node.getNumberOfInputs()):
+        inputs_number = node.getNumberOfInputs()
+        if inputs_number > 0:
+            self.inputs_widget.setHidden(False)
+        outputs_number = node.getNumberOfOutputs()
+        if outputs_number > 0:
+            self.outputs_widget.setHidden(False)
+
+        for _ in range(inputs_number):
             input_btn = QRoundButton()
             input_btn.setButtonSize(10)
             self.inputs_hbox.addWidget(input_btn)
 
-        for _ in range(node.getNumberOfOutputs()):
+        for _ in range(outputs_number):
             output_btn = QRoundButton()
             output_btn.setButtonSize(10)
             self.outputs_hbox.addWidget(output_btn)
--- tests/test_nodeWidget.py
+++ tests/test_nodeWidget.py
@@ -14,9 +14,19 @@ from UI.NodeWidget import NodeWidget
 from PluginLib.CompactQt.Qt import QApplication
 
 cube_node = NodesInfo.getNodes()[0]()
+cube_node._num_inputs = 2
+cube_node._num_outputs = 3
 
 app = QApplication([])
 window = NodeWidget()
+window.setStyleSheet(
+    """
+            QWidget {
+                background-color: #424242;
+                color: #c2c2c2;
+            }
+            """
+)
 window.setNode(cube_node)
 window.show()
 app.exec()
