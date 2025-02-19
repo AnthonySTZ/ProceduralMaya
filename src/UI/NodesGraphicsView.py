cbfc31d02e73931d66e8931a52eabccf28e1ade4
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-19 18:27:01 +0100

    remove red backgroud

--- src/UI/NodesGraphicsView.py
+++ src/UI/NodesGraphicsView.py
@@ -3,15 +3,5 @@ from PluginLib.CompactQt.Qt import QGraphicsView
 
 class NodesGraphicsView(QGraphicsView):
     def __init__(self, scene, parent=None):
+        self._scene = scene
         super().__init__(scene, parent)
-        self.buildUI()
-
-    def buildUI(self):
-        self.setStyleSheet(
-            """
-            QGraphicsView{
-                background-color: red;
-                border: 0;
-            }
-            """
-        )
