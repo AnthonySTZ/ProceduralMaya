2d496c61a25b42d1bcc3340fb8b38838f1852cf6
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-20 19:02:59 +0100

    subclass clickadble ellipse item to QGraphicsObject to handle signal

--- src/Core/Qt/AQClickableEllipseItem.py
+++ src/Core/Qt/AQClickableEllipseItem.py
@@ -1,6 +1,30 @@
-from PluginLib.CompactQt.Qt import QGraphicsEllipseItem
+from PluginLib.CompactQt.Qt import QRectF, SIGNAL, QGraphicsObject, QBrush, QPen
 
 
-class AQClickableEllipseItem(QGraphicsEllipseItem):
-    def __init__(self, pos_x, pos_y, width, height):
-        super().__init__(pos_x, pos_y, width, height)
+class AQClickableEllipseItem(QGraphicsObject):
+    clicked = SIGNAL(QGraphicsObject)
+
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self._rect = QRectF(x, y, width, height)
+        self._brush = QBrush()
+        self._pen = QPen()
+
+    def setBrush(self, brush):
+        self._brush = brush
+        self.update()
+
+    def setPen(self, pen):
+        self._pen = pen
+        self.update()
+
+    def boundingRect(self):
+        return self._rect
+
+    def paint(self, painter, option, widget=None):
+        painter.setBrush(self._brush)
+        painter.setPen(self._pen)
+        painter.drawEllipse(self._rect)
+
+    def mousePressEvent(self, event):
+        self.clicked.emit(self)
