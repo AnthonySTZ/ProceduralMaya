51b6ce0939819ef4261a5c5c962073db491a22c8
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-20 10:41:07 +0100

    move function to move in the graphicsview inside the new aqgraphicstransformview

--- src/Core/Qt/AQGraphicsTransformView.py
+++ src/Core/Qt/AQGraphicsTransformView.py
@@ -1,6 +1,31 @@
-from PluginLib.CompactQt.Qt import QGraphicsView
+from PluginLib.CompactQt.Qt import QGraphicsView, QPoint, Qt
 
 
 class AQGraphicsTransformView(QGraphicsView):
-    def __init__(self, parent=None):
-        super().__init__(parent)
+    def __init__(self, scene, parent=None):
+        super().__init__(scene, parent)
+        self._is_scrolling = False
+        self._scroll_pos = QPoint(0, 0)
+        self._prev_scale = 1.0
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MouseButton.MiddleButton:
+            self._is_scrolling = True
+            self._scroll_pos = event.position()
+        else:
+            super().mousePressEvent(event)
+
+    def mouseMoveEvent(self, event):
+        if self._is_scrolling:
+            new_pos = event.position()
+            offset = new_pos - self._scroll_pos
+            transform = self.transform()
+            self.translate(offset.x() / transform.m11(), offset.y() / transform.m22())
+            self._scroll_pos = new_pos
+        else:
+            super().mouseMoveEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if self._is_scrolling:
+            self._is_scrolling = False
+        super().mouseReleaseEvent(event)
