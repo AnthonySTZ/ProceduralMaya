2c2f207e1a87c560260a0ac1a07caa0b938a0061
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-20 10:41:23 +0100

    subcall nodesview to the new transforview

--- src/UI/NodesGraphicsView.py
+++ src/UI/NodesGraphicsView.py
@@ -7,18 +7,16 @@ from PluginLib.CompactQt.Qt import (
     QCursor,
     QAction,
     QPainter,
-    QPoint,
     QFrame,
 )
 from Core.Nodes.NodesInfo import NodesInfo
 from UI.NodeGraphicsItem import NodeGraphicsItem
+from Core.Qt.AQGraphicsTransformView import AQGraphicsTransformView
 
 
-class NodesGraphicsView(QGraphicsView):
+class NodesGraphicsView(AQGraphicsTransformView):
     def __init__(self, scene, parent=None):
         self._scene = scene
-        self._is_scrolling = False
-        self._scroll_pos = QPoint(0, 0)
         super().__init__(scene, parent)
         self.buildUI()
 
@@ -54,25 +52,3 @@ class NodesGraphicsView(QGraphicsView):
             item_pos = self.mapToScene(self.mapFromGlobal(QCursor.pos()))
             item = NodeGraphicsItem(item_pos, node)
             self._scene.addItem(item)
-
-    def mousePressEvent(self, event):
-        if event.button() == Qt.MouseButton.MiddleButton:
-            self._is_scrolling = True
-            self._scroll_pos = event.position()
-        else:
-            super().mousePressEvent(event)
-
-    def mouseMoveEvent(self, event):
-        if self._is_scrolling:
-            new_pos = event.position()
-            offset = new_pos - self._scroll_pos
-            transform = self.transform()
-            self.translate(offset.x() / transform.m11(), offset.y() / transform.m22())
-            self._scroll_pos = new_pos
-        else:
-            super().mouseMoveEvent(event)
-
-    def mouseReleaseEvent(self, event):
-        if self._is_scrolling:
-            self._is_scrolling = False
-        super().mouseReleaseEvent(event)
