b424c063160d163bed4693e57a8ad2f45a8939d9
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-20 18:39:32 +0100

    rewrite correct number of input and outputs for translate node

--- src/Core/Nodes/Transform.py
+++ src/Core/Nodes/Transform.py
@@ -5,8 +5,8 @@ class Transform(BaseNode):
     def __init__(self):
         super().__init__()
         self._name = "Transform"
-        self._num_inputs = 3
-        self._num_outputs = 2
+        self._num_inputs = 1
+        self._num_outputs = 1
         self._parameters = {"Translate": 0.0, "Rotate": 0.0, "Scale": 1.0}
 
     def getOutput(self, output_index):
