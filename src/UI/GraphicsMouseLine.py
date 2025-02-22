889d58465822c750094ad8a6bf64d376e4477773
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-22 17:55:11 +0100

    rename item in IO

--- src/UI/GraphicsMouseLine.py
+++ src/UI/GraphicsMouseLine.py
@@ -5,20 +5,20 @@ class GraphicsMouseLine(QGraphicsLineItem):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self._item = None
+        self._io = None
         self._mouse_pos = QPoint(0, 0)
         self.setPen(QPen(Qt.GlobalColor.white))
 
-    def setItem(self, item):
-        self._item = item
-        self._item.xChanged.connect(self.updateLine)
-        self._item.yChanged.connect(self.updateLine)
+    def setIO(self, io):
+        self._io = io
+        self._io.xChanged.connect(self.updateLine)
+        self._io.yChanged.connect(self.updateLine)
 
     def updateLine(self):
-        item_pos = self._item.centerPos()
+        io_pos = self._io.centerPos()
         self.setLine(
-            item_pos.x(),
-            item_pos.y(),
+            io_pos.x(),
+            io_pos.y(),
             self._mouse_pos.x(),
             self._mouse_pos.y(),
         )
