afdd70a4b319849d05144283bbf135c90b8a5b25
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-22 15:02:16 +0100

    update Line when item is moved

--- src/UI/GraphicsConnectionLine.py
+++ src/UI/GraphicsConnectionLine.py
@@ -5,22 +5,26 @@ class GraphicsConnectionLine(QGraphicsLineItem):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self._first_pos = None
-        self._last_pos = None
+        self._first_item = None
+        self._last_item = None
 
-    def setFirstPoint(self, pos):
-        self._first_pos = pos
+    def setFirstItem(self, item):
+        self._first_item = item
+        self._first_item.xChanged.connect(self.updateLine)
+        self._first_item.yChanged.connect(self.updateLine)
 
-    def setLastPoint(self, pos):
-        self._last_pos = pos
+    def setLastItem(self, item):
+        self._last_item = item
+        self._last_item.xChanged.connect(self.updateLine)
+        self._last_item.yChanged.connect(self.updateLine)
 
     def updateLine(self):
-        if self._first_pos is None or self._last_pos is None:
+        if self._first_item is None or self._last_item is None:
             raise ValueError("Line pos should not be None")
 
         self.setLine(
-            self._first_pos.x(),
-            self._first_pos.y(),
-            self._last_pos.x(),
-            self._last_pos.y(),
+            self._first_item.x(),
+            self._first_item.y(),
+            self._last_item.x(),
+            self._last_item.y(),
         )
