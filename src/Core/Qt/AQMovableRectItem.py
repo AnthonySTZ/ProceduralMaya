bbba6a8b787b6095fcc54491db62af284a3c177b
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-22 17:49:20 +0100

    create moveable rect

---
+++ src/Core/Qt/AQMovableRectItem.py
@@ -0,0 +1,57 @@
+from PluginLib.CompactQt.Qt import (
+    QPen,
+    SIGNAL,
+    QBrush,
+    QRectF,
+    QGraphicsObject,
+    QGraphicsItem,
+)
+
+
+class AQMovableRectItem(QGraphicsObject):
+    moved = SIGNAL()
+
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self._rect = QRectF(0, 0, width, height)
+        self.setPos(x, y)
+        self._brush = QBrush()
+        self._pen = QPen()
+        self._user_data = {}
+        self.setFlag(
+            QGraphicsItem.GraphicsItemFlag.ItemSendsGeometryChanges
+            | QGraphicsItem.GraphicsItemFlag.ItemIsMovable,
+            True,
+        )
+
+    def setUserData(self, key, value):
+        self._user_data[key] = value
+
+    def getUserData(self, key):
+        return self._user_data[key]
+
+    def setBrush(self, brush):
+        self._brush = brush
+        self.update()
+
+    def setPen(self, pen):
+        self._pen = pen
+        self.update()
+
+    def centerPos(self):
+        center_local = self._rect.center()
+        center_scene = self.mapToScene(center_local)
+        return center_scene
+
+    def boundingRect(self):
+        return self._rect
+
+    def paint(self, painter, option, widget=None):
+        painter.setBrush(self._brush)
+        painter.setPen(self._pen)
+        painter.drawRect(self._rect)
+
+    def itemChange(self, change, value):
+        if change == QGraphicsItem.GraphicsItemChange.ItemPositionChange:
+            self.moved.emit()
+        return super().itemChange(change, value)
