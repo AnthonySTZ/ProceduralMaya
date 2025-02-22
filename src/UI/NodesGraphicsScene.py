d7f909caaf7c54b58e59054253657fa61396d010
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-22 16:15:26 +0100

    store nodeitem

--- src/UI/NodesGraphicsScene.py
+++ src/UI/NodesGraphicsScene.py
@@ -11,21 +11,23 @@ class NodesGraphicsScene(QGraphicsScene):
         super().__init__(parent)
         self._selecting = None
         self._io_selected = None
+        self._selected_node = None
         self._current_connection = None
 
     def addNode(self, node):
         self.addItem(node)
-        node.inputClicked.connect(self.inputClicked)
+        node.inputClicked.connect(lambda input: self.inputClicked(node, input))
         node.outputClicked.connect(
             lambda item: print(
                 "Output index " + str(item.getUserData("index")) + " clicked"
             )
         )
 
-    def inputClicked(self, input):
-        self.ioClicked(input, self.INPUT)
+    def inputClicked(self, node_item, input):
+        print(node_item)
+        self.ioClicked(node_item, input, self.INPUT)
 
-    def ioClicked(self, io, iotype):
+    def ioClicked(self, node_item, io, iotype):
         if self._selecting == iotype:  # Cannot link io type to the same type
             self._current_connection.setParentItem(None)
             self._current_connection = None
@@ -35,6 +37,7 @@ class NodesGraphicsScene(QGraphicsScene):
         if self._selecting is None:  # First click on io
             self.createConnection()
             self._selecting = iotype
+            self._selected_node = node_item
             self._current_connection.setFirstItem(io)
             return
 
