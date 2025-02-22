1e791214063ec23a4514a7f78a59fc6ebe3ba176
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-22 16:28:39 +0100

    create logic in scene for linking

--- src/UI/NodesGraphicsScene.py
+++ src/UI/NodesGraphicsScene.py
@@ -1,5 +1,6 @@
 from PluginLib.CompactQt.Qt import QGraphicsScene
 from UI.GraphicsConnectionLine import GraphicsConnectionLine
+from UI.IOItem import IOItem
 
 
 class NodesGraphicsScene(QGraphicsScene):
@@ -15,6 +16,23 @@ class NodesGraphicsScene(QGraphicsScene):
     def ioClicked(self, io_item):
         print(io_item.getNodeItem())
         print(io_item.getType())
+
+        if self._current_io is None:  # First Click
+            self._current_io = io_item
+            return
+
+        if (
+            self._current_io.getType() == io_item.getType()
+        ):  # Click on same type as current selected
+            return
+
+        # TODO: Create connection between input and output
+        print(
+            "Connection between "
+            + io_item.getNodeItem().title_name.toPlainText()
+            + " and "
+            + io_item.getNodeItem().title_name.toPlainText()
+        )
 
     def createConnection(self):
         self._current_connection = GraphicsConnectionLine()
