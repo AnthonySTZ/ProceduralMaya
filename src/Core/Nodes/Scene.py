66149bef5a8c8720090cadd65d720658334041b9
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-23 21:01:32 +0100

    add comment to Scene

--- src/Core/Nodes/Scene.py
+++ src/Core/Nodes/Scene.py
@@ -3,6 +3,9 @@ class Scene:
         self._nodes = {}
 
     def addNode(self, node):
+        """
+        Adds a node to the scene. Renames the node if a node with the same name already exists in the scene
+        """
         node_name = node.getName()
         if node_name not in self._nodes:
             self._nodes[node_name] = node
@@ -13,6 +16,9 @@ class Scene:
         self._nodes[new_name] = node
 
     def getUntakenName(self, name):
+        """
+        Tries every possibilites of name (pattern: name1, name2,...) and return the first that not exists in the scene.
+        """
         i = 1
         while name + str(i) in self._nodes:
             i += 1
