535eed1d74acb5f539e678cdd5e2e93258be6c46
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-23 10:16:37 +0100

    add comments to nodeConnections

--- src/Core/Nodes/NodeConnections.py
+++ src/Core/Nodes/NodeConnections.py
@@ -1,3 +1,10 @@
 class NodeConnections:
+    """
+    Store all connections using a dictionary
+
+    key : Input -> Can avoid input being linked 2 times
+    value: Output
+    """
+
     def __init__(self):
         self._connections = {}
