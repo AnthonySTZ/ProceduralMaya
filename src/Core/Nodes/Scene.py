2d96a89a59f30012a17c9735d1e03f65122d0300
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-23 20:55:31 +0100

    handle addNode when node name is already in scene

--- src/Core/Nodes/Scene.py
+++ src/Core/Nodes/Scene.py
@@ -6,3 +6,14 @@ class Scene:
         node_name = node.getName()
         if node_name not in self._nodes:
             self._nodes[node_name] = node
+            return
+
+        new_name = self.getUntakenName(node_name)
+        node.setName(new_name)
+        self._nodes[new_name] = node
+
+    def getUntakenName(self, name):
+        i = 1
+        while name + str(i) in self._nodes:
+            i += 1
+        return name + str(i)
--- src/Core/Nodes/tests/test_nodes.py
+++ src/Core/Nodes/tests/test_nodes.py
@@ -3,12 +3,24 @@ from load_plugin import load_plugin
 load_plugin()
 from Core.Nodes.Cube import Cube
 from Core.Nodes.Transform import Transform
+from Core.Nodes.Scene import Scene
+
+scene = Scene()
 
 cube_01 = Cube()
 cube_02 = Cube()
+cube_03 = Cube()
 transform_01 = Transform()
 
-transform_01.setInput(0, cube_01)
+scene.addNode(cube_01)
+scene.addNode(cube_02)
+scene.addNode(cube_03)
+scene.addNode(transform_01)
 
-print(transform_01.inputConnection(0))
-print(transform_01.input(0))
+print(cube_02.getName())
+print(cube_03.getName())
+
+# transform_01.setInput(0, cube_01)
+
+# print(transform_01.inputConnection(0))
+# print(transform_01.input(0))
