34388300c9a0192698bff653a7f26134c907fa4c
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-23 18:42:21 +0100

    add comments in BaseNode

--- src/Core/Nodes/BaseNode.py
+++ src/Core/Nodes/BaseNode.py
@@ -30,11 +30,19 @@ class BaseNode:
         self._parameters[key] = value
 
     def setInput(self, input_index, input_node, output_index=0):
-        if input_index >= self._num_inputs:
+        """
+        Creates a connection between this node and another.
+
+        input_index : index of the input of this node.
+        input_node : node that the connection comes from.
+        output_index : index of the input of the input_node.
+        """
+
+        if input_index >= self._num_inputs:  # input out of range
             print("Not enough inputs for " + self.getName() + " !")
             return
 
-        if output_index >= input_node.getNumberOfOutputs():
+        if output_index >= input_node.getNumberOfOutputs():  # input out of range
             print("Not enough outputs for " + input_node.getName() + " !")
             return
 
@@ -43,12 +51,22 @@ class BaseNode:
         )
 
     def inputConnection(self, index):
+        """
+        Returns the NodeConnection at input index.
+        If no input connection found, returns None.
+        """
+
         if index not in self._inputs:
             return None
 
         return self._inputs[index]
 
     def input(self, index):
+        """
+        Returns the Node at input index.
+        If no input connection found, returns None.
+        """
+
         if index not in self._inputs:
             return None
 
