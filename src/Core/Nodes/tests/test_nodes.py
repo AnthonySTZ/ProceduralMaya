80c75cdee86d5476c003d942ac7bd2d6a0f7e669
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-24 14:35:28 +0100

    test number of input on cube

--- src/Core/Nodes/tests/test_nodes.py
+++ src/Core/Nodes/tests/test_nodes.py
@@ -1,24 +1,10 @@
-from load_plugin import load_plugin
+from .load_plugin import load_plugin
 
 load_plugin()
 from Core.Nodes.Cube import Cube
 from Core.Nodes.Transform import Transform
-from Core.Nodes.Scene import Scene
 
-scene = Scene()
 
-cube_01 = Cube()
-cube_02 = Cube()
-transform_01 = Transform()
-
-scene.addNode(cube_01)
-scene.addNode(cube_02)
-scene.addNode(transform_01)
-
-print(cube_02.getName())
-print(scene.getNodes())
-
-# transform_01.setInput(0, cube_01)
-
-# print(transform_01.inputConnection(0))
-# print(transform_01.input(0))
+def test_cube_inputs_number_should_be_zero():
+    node = Cube()
+    assert node.getNumberOfInputs() == 0
