8246987aa2f3fecff7ab6c0286b8c4797cff47e9
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-24 18:40:50 +0100

    add amount and width to inputsoutputs

--- src/UI/Elements/InputsOuputs.py
+++ src/UI/Elements/InputsOuputs.py
@@ -1,11 +1,17 @@
 from PluginLib.CompactQt.Qt import QGraphicsObject
+from Core.Qt.AQClickableEllipseItem import AQClickableEllipseItem
 
 
 class InputsOutputs(QGraphicsObject):
     INPUT = 0
     OUTPUT = 1
 
-    def __init__(self, type):
+    def __init__(self, type, amount, width):
         super().__init__()
         self._type = type
+        self._amount = amount
+        self._width = width
         self.buildUI()
+
+    def buildUI(self):
+        pass
