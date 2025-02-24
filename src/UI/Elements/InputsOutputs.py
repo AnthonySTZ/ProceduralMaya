cb5082fcd1772f69e6aa5ccfa29d98358046007f
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-24 19:06:20 +0100

    add a 0 amount check

--- src/UI/Elements/InputsOutputs.py
+++ src/UI/Elements/InputsOutputs.py
@@ -1,4 +1,4 @@
-from PluginLib.CompactQt.Qt import QGraphicsObject, QRectF
+from PluginLib.CompactQt.Qt import QGraphicsObject, QRectF, QPen, QBrush, Qt
 from Core.Logic import logics
 from Core.Qt.AQClickableEllipseItem import AQClickableEllipseItem
 from UI.Elements.NodeRect import NodeRect
@@ -11,28 +11,32 @@ class InputsOutputs(QGraphicsObject):
 
     RADIUS = 9
 
-    def __init__(self, parent, type, amount, width):
+    def __init__(self, parent, type, amount):
         super().__init__()
-        self._rect = QRectF(0, 0, width, self.RADIUS * 2)
+        self._rect = QRectF(0, 0, NodeRect.WIDTH, self.RADIUS * 2)
         self.setParentItem(parent)
         self._type = type
         self._amount = amount
-        self._width = width
         self.buildUI()
 
     def buildUI(self):
+        if self._amount == 0:
+            return
+
         height = 0
         if self._type == self.INPUT:
-            height = -self.PADDING
+            height = -self.PADDING - self.RADIUS
         else:
             height = NodeRect.HEIGHT + self.PADDING
 
         points_offsets = logics.evenly_distribute_point_on_line(
-            self._width, self._amount
+            NodeRect.WIDTH, self._amount
         )
         for offset in points_offsets:
             ellipse = AQClickableEllipseItem(offset, height, self.RADIUS, self.RADIUS)
-            ellipse.setParentItem(self._rect)
+            ellipse.setBrush(QBrush(Qt.GlobalColor.gray))
+            ellipse.setPen(QPen(Qt.GlobalColor.black))
+            ellipse.setParentItem(self)
 
     def boundingRect(self):
         return self._rect
