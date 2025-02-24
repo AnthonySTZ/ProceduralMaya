06d15512c8ce1a0322d457caa74ef9b5865b3f1d
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-24 14:36:04 +0100

    test cube outputs

--- src/Core/Nodes/tests/test_nodes.py
+++ src/Core/Nodes/tests/test_nodes.py
@@ -8,3 +8,8 @@ from Core.Nodes.Transform import Transform
 def test_cube_inputs_number_should_be_zero():
     node = Cube()
     assert node.getNumberOfInputs() == 0
+
+
+def test_cube_outputs_number_should_be_one():
+    node = Cube()
+    assert node.getNumberOfOutputs() == 1
