febdf1a3a87ce0f1f68c6d8ccf1eceb6ae878e15
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-24 18:52:24 +0100

    create basic paint and bouding rect for inputoutputs

--- src/UI/Elements/InputsOuputs.py
+++ src/UI/Elements/InputsOuputs.py
@@ -1,4 +1,5 @@
-from PluginLib.CompactQt.Qt import QGraphicsObject
+from PluginLib.CompactQt.Qt import QGraphicsObject, QRectF
+from Core.Logic import logics
 from Core.Qt.AQClickableEllipseItem import AQClickableEllipseItem
 
 
@@ -6,12 +7,23 @@ class InputsOutputs(QGraphicsObject):
     INPUT = 0
     OUTPUT = 1
 
+    RADIUS = 9
+
     def __init__(self, type, amount, width):
         super().__init__()
+        self._rect = QRectF(0, 0, width, self.RADIUS * 2)
         self._type = type
         self._amount = amount
         self._width = width
         self.buildUI()
 
     def buildUI(self):
-        pass
+        points_offsets = logics.evenly_distribute_point_on_line(
+            self._width, self._amount
+        )
+
+    def boundingRect(self):
+        return self._rect
+
+    def paint(self, painter, option, widget=None):
+        return
