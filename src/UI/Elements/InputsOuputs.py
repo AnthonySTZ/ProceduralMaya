835db73c8b3e1acf24e6b5ed15eb2e7a1d83dc09
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-24 18:35:22 +0100

    rename inputoutput to inputsoutputs

--- src/UI/Elements/InputsOuputs.py
+++ src/UI/Elements/InputsOuputs.py
@@ -1,10 +1,11 @@
 from PluginLib.CompactQt.Qt import QGraphicsObject
 
 
-class InputOutput(QGraphicsObject):
+class InputsOutputs(QGraphicsObject):
     INPUT = 0
     OUTPUT = 1
 
     def __init__(self, type):
         super().__init__()
         self._type = type
+        self.buildUI()
