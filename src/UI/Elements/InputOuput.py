50188971635fb4b4281e7a79ff5a88e2edeb69ba
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-24 18:34:51 +0100

    set io type

--- src/UI/Elements/InputOuput.py
+++ src/UI/Elements/InputOuput.py
@@ -2,5 +2,9 @@ from PluginLib.CompactQt.Qt import QGraphicsObject
 
 
 class InputOutput(QGraphicsObject):
-    def __init__(self):
+    INPUT = 0
+    OUTPUT = 1
+
+    def __init__(self, type):
         super().__init__()
+        self._type = type
