302d4d7f1c8680b63b46498fb13c811f8f01cfa2
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-25 11:13:27 +0100

    refactor update when nodes move

--- src/UI/Elements/ConnectionLine.py
+++ src/UI/Elements/ConnectionLine.py
@@ -13,17 +13,12 @@ class ConnectionLine(QGraphicsLineItem):
         """
         self._first_item = io_item_1
         self._last_item = io_item_2
-        self._first_item.getNodeItem().moved.connect(
-            lambda: self.updateLine(
-                self._first_item.centerPos(), self._last_item.centerPos()
-            )
-        )
-        self._last_item.getNodeItem().moved.connect(
-            lambda: self.updateLine(
-                self._first_item.centerPos(), self._last_item.centerPos()
-            )
-        )
+        self._first_item.getNodeItem().moved.connect(self.updateWhenNodesMove)
+        self._last_item.getNodeItem().moved.connect(self.updateWhenNodesMove)
         self.updateLine(io_item_1.centerPos(), io_item_2.centerPos())
+
+    def updateWhenNodesMove(self):
+        self.updateLine(self._first_item.centerPos(), self._last_item.centerPos())
 
     def createMovableConnectionFromOneNode(self, io_item):
         self._node_item = io_item
