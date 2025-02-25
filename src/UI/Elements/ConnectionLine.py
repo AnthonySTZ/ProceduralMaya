4d7b6bd66c4f87bfb1c0dc85bf2f9f00f043de2e
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-25 10:43:32 +0100

    create basic function in ConnectionLine

--- src/UI/Elements/ConnectionLine.py
+++ src/UI/Elements/ConnectionLine.py
@@ -1,6 +1,23 @@
-from PluginLib.CompactQt.Qt import QGraphicsLineItem
+from PluginLib.CompactQt.Qt import QGraphicsLineItem, QPen, Qt
 
 
 class ConnectionLine(QGraphicsLineItem):
     def __init__(self):
         super().__init__()
+        self.setPen(QPen(Qt.GlobalColor.white))
+        self.setZValue(-1)
+
+    def createConnectionBetweenNodes(self, node_item_1, node_item_2):
+        self._first_item = node_item_1
+        self._last_item = node_item_2
+
+    def createMovableConnectionFromOneNode(self, node_item):
+        self._node_item = node_item
+
+    def updateLine(self, pos_1, pos_2):
+        self.setLine(
+            pos_1.x(),
+            pos_1.y(),
+            pos_2.x(),
+            pos_2.y(),
+        )
