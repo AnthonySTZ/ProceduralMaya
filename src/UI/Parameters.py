218402cf903a26a1793eaa0eb93cb11fa6f22498
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-25 17:47:39 +0100

    change parameters directory

--- src/UI/Parameters.py
+++ src/UI/Parameters.py
@@ -1,4 +1,4 @@
-from PluginLib.CompactQt.Qt import QWidget, QHBoxLayout
+from PluginLib.CompactQt.Qt import QWidget, QVBoxLayout, QLabel
 
 
 class Parameters(QWidget):
@@ -8,15 +8,22 @@ class Parameters(QWidget):
         self.buildUI()
 
     def buildUI(self):
-        self._hbox = QHBoxLayout()
+        self._hbox = QVBoxLayout()
         self.setLayout(self._hbox)
 
     def setNode(self, node):
         self._node = node
 
     def updateParameters(self):
+        self.clearParams()
+
         if self._node is None:
             return
 
         for param, value in self._node.getParameters():
-            print(param)
+            label = QLabel(param)
+            self._hbox.addWidget(label)
+
+    def clearParams(self):
+        for child in self._hbox.children():
+            child.setParent(None)
