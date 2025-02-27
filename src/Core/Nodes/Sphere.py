daf41c12a2d16cab7dc16072429dee34b1d08764
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-27 10:44:46 +0100

    use mel for sphere

--- src/Core/Nodes/Sphere.py
+++ src/Core/Nodes/Sphere.py
@@ -4,7 +4,7 @@ from Core.Field.Float3 import Float3
 from Core.Field.Int import Int
 
 try:
-    import maya.cmds as mc  # type: ignore
+    import maya.mel as mel  # type: ignore
 except:
     pass
 
@@ -28,11 +28,14 @@ class Sphere(BaseNode):
             print("Index out of range !")
             return
 
-        axis = self._parameters["Axis"].toList()
-        radius = self._parameters["Radius"].value
-        sub_a = self._parameters["Subdivisions Axis"].value
-        sub_h = self._parameters["Subdivisions Height"].value
+        command = self.createSphereMelCommand()
+        return mel.eval(command)
 
-        mc.nurbsToPolygonsPref()
+    def createSphereMelCommand(self):
+        axis = " -ax " + self._parameters["Axis"].toStr()
+        radius = " -r " + self._parameters["Radius"].toStr()
+        sub_a = " -sx " + self._parameters["Subdivisions Axis"].toStr()
+        sub_h = " -sy " + self._parameters["Subdivisions Height"].toStr()
 
-        return mc.polySphere(ax=axis, r=radius, sx=sub_a, sy=sub_h)
+        command = "polySphere" + axis + radius + sub_a + sub_h + ";"
+        return command
