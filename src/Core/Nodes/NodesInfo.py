54b4dd8750aa08937a55e6835cc6e6455545332b
AnthonySTZ <anthonycandy2309@gmail.com>
2025-02-27 15:35:21 +0100

    add merge to nodeInfos

--- src/Core/Nodes/NodesInfo.py
+++ src/Core/Nodes/NodesInfo.py
@@ -5,7 +5,8 @@ class NodesInfo:
         from Core.Nodes.Cube import Cube
         from Core.Nodes.Sphere import Sphere
         from Core.Nodes.Transform import Transform
+        from Core.Nodes.Merge import Merge
         from Core.Nodes.Null import Null
 
-        nodes = [Cube, Sphere, Transform, Null]
+        nodes = [Cube, Sphere, Transform, Merge, Null]
         return nodes
