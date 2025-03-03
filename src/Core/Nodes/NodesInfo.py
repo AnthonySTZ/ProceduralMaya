5fcd014b5b4af95e6b186ec40c7aaf1c41484652
AnthonySTZ <anthonycandy2309@gmail.com>
2025-03-03 22:12:32 +0100

    add duplicate node to the nodeinfos

--- src/Core/Nodes/NodesInfo.py
+++ src/Core/Nodes/NodesInfo.py
@@ -5,8 +5,9 @@ class NodesInfo:
         from Core.Nodes.Cube import Cube
         from Core.Nodes.Sphere import Sphere
         from Core.Nodes.Transform import Transform
+        from Core.Nodes.Duplicate import Duplicate
         from Core.Nodes.Merge import Merge
         from Core.Nodes.Null import Null
 
-        nodes = [Cube, Sphere, Transform, Merge, Null]
+        nodes = [Cube, Sphere, Transform, Duplicate, Merge, Null]
         return nodes
