425082f215aaa599759b015c9d850cf66ee75a1f
AnthonySTZ <anthonycandy2309@gmail.com>
2025-03-10 12:38:12 +0100

    add subd for cube

--- src/Core/Nodes/Cube.py
+++ src/Core/Nodes/Cube.py
@@ -1,5 +1,6 @@
 from .BaseNode import BaseNode
 from Core.Field.UnsignedFloat import UnsignedFloat
+from Core.Field.UnsignedInt import UnsignedInt
 
 try:
     import maya.cmds as mc  # type: ignore
@@ -18,6 +19,9 @@ class Cube(BaseNode):
             "width": UnsignedFloat(1.0),
             "height": UnsignedFloat(1.0),
             "depth": UnsignedFloat(1.0),
+            "Sub Width": UnsignedInt(1),
+            "Sub Height": UnsignedInt(1),
+            "Sub Depth": UnsignedInt(1),
         }
 
     def commandAtIndex(self, index):
@@ -32,5 +36,15 @@ class Cube(BaseNode):
         width = self._parameters["width"].value
         height = self._parameters["height"].value
         depth = self._parameters["depth"].value
-        cube_obj = mc.polyCube(w=width, h=height, d=depth)[0]
+        sub_w = self._parameters["Sub Width"].value
+        sub_h = self._parameters["Sub Height"].value
+        sub_d = self._parameters["Sub Depth"].value
+        cube_obj = mc.polyCube(
+            w=width,
+            h=height,
+            d=depth,
+            subdivisionsWidth=sub_w,
+            subdivisionsHeight=sub_h,
+            subdivisionsDepth=sub_d,
+        )[0]
         return cube_obj
