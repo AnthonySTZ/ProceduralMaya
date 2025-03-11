121c0fff14552c64603c32f550287cb1c32dd302
AnthonySTZ <anthonycandy2309@gmail.com>
2025-03-11 17:08:52 +0100

    add fit_value in logics

--- src/Core/Logic/logics.py
+++ src/Core/Logic/logics.py
@@ -1,3 +1,7 @@
 def evenly_distribute_point_on_line(line_length, pt_nb):
     dt = line_length / pt_nb
     return [0.5 * dt + i * dt for i in range(pt_nb)]
+
+
+def fit_value(value, r_min, r_max, t_min, t_max):
+    return (value - r_min) / (r_max - r_min) * (t_max - t_min) + t_min
